bf0788ed70308080b849cc4c00550165d6e3723e
Matthew Watman <mattwatman@gmail.com>
2021-06-22 11:39:26 -0400

    Add css import and element classes

--- components/Layout/header.js
+++ components/Layout/header.js
@@ -1,14 +1,15 @@
 import { Fragment }from 'react'
 
 import mealsImage from '../../assets/meals.jpg'
+import classes from './Header.module.css'
 
 const Header = props => {
     return <Fragment>
-        <header>
+        <header className={classes.Header}>
             <h1>ReactMeals</h1>
             <button>Cart</button>
         </header>
-        <div>
+        <div className={classes['main-image']}>
             <img src={mealsImage} alt="A table full of delicious food"/>
         </div>
     </Fragment>
