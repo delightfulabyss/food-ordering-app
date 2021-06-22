32c5f3fbc51ab6b35d933ecb5d8f6961208c7d9c
Matthew Watman <mattwatman@gmail.com>
2021-06-22 16:03:17 -0400

    Add styles

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import CartIcon from '../Cart/CartIcon'
+import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
     return (
         <button>
-          <span></span>  
-          <span></span>  
-          <span></span>   
+          <span className={classes.button}><CartIcon/></span>  
+          <span className={classes.icon}>Your Cart</span>  
+            <span className={classes.badge}>
+            3
+            </span>
         </button>
     )
 }
