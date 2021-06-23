2eb477ffce38cf541c574cfba9e603ddbe024bf9
Matthew Watman <mattwatman@gmail.com>
2021-06-23 13:59:07 -0400

    Add carticon and headercartbutton components

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,9 +4,11 @@ import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
     return (
-        <button>
-          <span className={classes.button}><CartIcon/></span>  
-          <span className={classes.icon}>Your Cart</span>  
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+          <span>Your Cart</span>  
             <span className={classes.badge}>
             3
             </span>
