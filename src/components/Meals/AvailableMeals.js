c70a81dee4f5f9df16feaa9f6b185d551f0fe7aa
Matthew Watman <mattwatman@gmail.com>
2021-06-23 15:40:18 -0400

    Wrap available meals component in card

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card'
 
 const DUMMY_MEALS = [
   {
@@ -31,12 +32,12 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map((meal) =>  <li>{meal.name}</li>)
     return (
-        <section className={classes.meals}>
-            <ul>
-               {mealsList} 
-            </ul>
-        </section>
-    )
+      <section className={classes.meals}>
+        <Card>
+          <ul>{mealsList}</ul>
+        </Card>
+      </section>
+    );
 }
 
 export default AvailableMeals
