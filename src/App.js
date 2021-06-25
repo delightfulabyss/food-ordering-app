8653511eca25a89496f8bb5bd5c023cdbbd0c5a2
Matthew Watman <mattwatman@gmail.com>
2021-06-25 16:12:52 -0400

    Provide cart context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react"
+import { useState } from "react"
 import Header from "./components/Layout/Header"
 import Meals from "./components/Meals/Meals"
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
 
 function App() {
 
@@ -16,13 +17,13 @@ function App() {
   }
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
