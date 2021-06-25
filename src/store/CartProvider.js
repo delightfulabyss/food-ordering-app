5d06dcba2709459c80f815e5e72d65098cb0f4f3
Matthew Watman <mattwatman@gmail.com>
2021-06-25 16:20:17 -0400

    Fix name error

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -4,14 +4,14 @@ import cartContext from './CartContext'
 const CartProvider = (props) => {
     const addItemToCartHandler = (item) => {}
     const removeItemFromCartHandler = (itemId) => {}
-    const cartContext = {
+    const cart_context = {
       items: [],
       totalAmount: 0,
       addItem: addItemToCartHandler,
       removeItem: removeItemFromCartHandler
     };
     return (
-        <cartContext.provider value={cartContext}>
+        <cartContext.provider value={cart_context}>
             {props.children}
         </cartContext.provider>
     )
