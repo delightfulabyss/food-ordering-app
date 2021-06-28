76b2f58c79b08b3a5388a7d6d51b5422aab3c85f
Matthew Watman <mattwatman@gmail.com>
2021-06-28 10:22:59 -0400

    Consume cart context in cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal'
+import CartContext from '../../store/CartContext'
 
 const Cart = (props) => {
-    const cartItems = <ul className={classes['cart-items']}>{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (<li>{item.name}</li>))}</ul>
+    const cartCTX = useContext(CartContext)
+    const totalAmount = `$${cartCTX.totalAmount.toFixed(2)}`
+    const hasItems = cartCTX.items.length > 0 
+
+    const cartItems = <ul className={classes['cart-items']}>{cartCTX.items.map((item) => (<li>{item.name}</li>))}</ul>
     return (
         <Modal onHideCart={props.onHideCart}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     )
