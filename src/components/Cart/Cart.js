a8f1937a8fc9f97ae5d54689eb3366c8ef1a0d2a
Matthew Watman <mattwatman@gmail.com>
2021-06-28 12:32:16 -0400

    Add item removal logic to cart reducer

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,7 +14,7 @@ const Cart = (props) => {
     }
 
     const cartItemAddHandler = (item) => {
-        
+        cartCTX.addItem({...item, amount: 1})
     }
 
     const cartItems = <ul className={classes['cart-items']}>
