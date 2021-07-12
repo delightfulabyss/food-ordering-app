09e09700f12faf4ebf9a85eee3d022fbadb5df38
Matthew Watman <mattwatman@gmail.com>
2021-07-12 09:24:51 -0400

    Add button functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,34 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/CartContext'
 
 const HeaderCartButton = props => {
-    const cartCtx = useContext(CartContext)
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
+    const cartCTX = useContext(CartContext)
+    const { items } = cartCTX
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount
     }, 0)
+  
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+  
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true)
+    const timer  = setTimeout(() => {
+      setBtnIsHighlighted(false)
+
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
     return (
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
