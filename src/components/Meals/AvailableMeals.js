c939cf22d8dfc9985c7cf319c63f7d866a430cb1
Matthew Watman <mattwatman@gmail.com>
2021-07-13 21:38:06 -0400

    Remformate meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import classes from './AvailableMeals.module.css'
-import Card from '../UI/Card'
-import MealItem from './MealItem/MealItem'
-
+import React, { useState, useEffect } from "react";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) =>
+  const [fetchedMeals, setFetchedMeals] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://meal-ordering-app-1aa10-default-rtdb.firebaseio.com/",
+    {headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }})
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+  const mealsList = fetchedMeals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
-    />)
-    return (
-      <section className={classes.meals}>
-        <Card>
-          <ul>{mealsList}</ul>
-        </Card>
-      </section>
-    );
-}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
 
-export default AvailableMeals
+export default AvailableMeals;
